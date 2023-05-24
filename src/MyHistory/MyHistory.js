ade4f47a35dd387f7d169b638591ee0a380e52ef
Omer Aplatony <omerap12@gmail.com>
2023-05-24 14:42:27 +0300

    Getting videos from server

--- src/MyHistory/MyHistory.js
+++ src/MyHistory/MyHistory.js
@@ -1,65 +1,90 @@
 import './MyHistory.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import LobbyPage from '../LobbyPage/LobbyPage';
+import { SERVER_URL } from '../config'
 
 
 const MyHistory = (props) => {
+  const [ShowLobby, setShowLobby] = useState(false);
+  const [fallHistory, setFallHistory] = useState([]);
 
-const [ShowLobby, setShowLobby] = useState(false);
+  const username = "omerap12"
+  useEffect(() => {
+    get_falls_history();
+  }, []);
+  
+  const get_falls_history = () => {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ username: username }),
+    };
+    fetch(`${SERVER_URL}/all_history`, requestOptions)
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json();
+        } else {
+          console.log('No no');
+        }
+      })
+      .then((data) => {
+        setFallHistory(data);
+        console.log(data)
+      })
+      .catch((error) => {
+        console.log('Error:', error);
+      });
+  };
+  
+  
+  
+  if (ShowLobby) {
+    return <LobbyPage username={props.username} password={props.password} BirthDay={props.BirthDay} Email={props.Email}/>;
+  }
+  
 
-if (ShowLobby) {
-  return <LobbyPage username={props.username} password={props.password} BirthDay={props.BirthDay} Email={props.Email}/>;
-}
+  const handleLobbyClick = (event) => {
+    event.preventDefault();
+    setShowLobby(true);
+  };
 
-const handleLobbyClick = (event) => {
-  event.preventDefault();
-  setShowLobby(true);
-};
-
-    return (
-        
-        <div className='main-div'>
-        <div>
-        <div className='myContacts-header'>My History</div>
-        <div className='logo-img' style={{width:'120px', height:'120px', marginTop:'-100px',borderRadius:'16px'}}>
-            </div>
-        </div>
-        <table class="table table-hover" className='myTable-history' style={{alignContent:'center', hover:'20'}}>
-  <thead>
-    <tr className='firstRow'>
-      <th scope="col">#</th>
-      <th scope="col">Date</th>
-      <th scope="col">Time</th>
-      <th scope="col">who cared</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr className='rows'>
-      <th scope="row">1</th>
-      <td>22/3</td>
-      <td>09:00</td>
-      <td>Omer</td>
-    </tr>
-    <tr className='rows'>
-      <th scope="row">2</th>
-      <td>5/7</td>
-      <td>09:00</td>
-      <td>Aviya</td>
-    </tr>
-    <tr className='rows'>
-      <th scope="row">3</th>
-      <td>25/8</td>
-      <td>09:00</td>
-      <td>Avital</td>
-    </tr>
-  </tbody>
-</table>
-<div className="footer">
-<p className='BackToLobbyFromHistory'>Back to <a href= "/lobby" style={{color: 'black'}} onClick={handleLobbyClick}> Lobby</a></p>
-</div>
-        </div>
-        
-    );
+return (
+  <div className="main-div">
+      <div>
+        <div className="myContacts-header">My History</div>
+        <div
+          className="logo-img"
+          style={{ width: '120px', height: '120px', marginTop: '-100px', borderRadius: '16px' }}
+        ></div>
+      </div>
+      <table className="table table-hover myTable-history" style={{ alignContent: 'center', hover: '20' }}>
+        <thead>
+          <tr className="firstRow">
+            <th scope="col">#</th>
+            <th scope="col">Date</th>
+            <th scope="col">Time</th>
+            <th scope="col">Who Cared</th>
+          </tr>
+        </thead>
+        <tbody>
+          {fallHistory.map((fall, index) => (
+            <tr className="rows" key={index}>
+              <th scope="row">{index + 1}</th>
+              <td>{fall.date}</td>
+              <td>{fall.date.split(' ')[1]}</td>
+              <td>{fall.filename.split('#')[1].split('.')[0]}</td>
+              <td><a href={fall.video_url}target='_blank'>Watch Video</a></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="footer">
+        <p className="BackToLobbyFromHistory">
+          Back to <a href="/lobby" style={{ color: 'black' }} onClick={handleLobbyClick}>Lobby</a>
+        </p>
+      </div>
+    </div>
+);
 };
 
 export default MyHistory;
